bb6943aee71809ca33a7c4d8e982d6f810bac487
Nurdoidz <nurdoidz@live.com>
2024-01-24 20:06:00 -0700

    Fix keymap for completion confirm

--- lua/setup/cmp.lua
+++ lua/setup/cmp.lua
@@ -25,7 +25,7 @@ cmp.setup {
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete {},
-        ['<C-;>'] = cmp.mapping.confirm {
+        ["<C-]>"] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
         },
